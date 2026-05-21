e67a7c57eb10d101543b4172d9ac1f5110143323
YaraHamza22 <yaraassadhamza@gmail.com>
2026-05-21 11:09:20 +0300

    Create Laravel framework cache directories on startup

--- docker/render-web.sh
+++ docker/render-web.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-mkdir -p storage/logs bootstrap/cache
+mkdir -p \
+  storage/logs \
+  storage/framework/cache/data \
+  storage/framework/sessions \
+  storage/framework/testing \
+  storage/framework/views \
+  bootstrap/cache
 touch storage/logs/laravel.log
 
 php artisan storage:link || true
