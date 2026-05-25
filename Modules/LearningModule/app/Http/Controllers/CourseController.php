e6480f539b41f86bd1af692dc4284163fdc4afe0
YaraHamza22 <yaraassadhamza@gmail.com>
2026-05-25 09:56:31 +0300

    fix

--- Modules/LearningModule/app/Http/Controllers/CourseController.php
+++ Modules/LearningModule/app/Http/Controllers/CourseController.php
@@ -124,7 +124,11 @@ class CourseController extends Controller
         }
 
         if ($request->is('api/v1/courses')) {
-            return $query->enrollable();
+            return $query
+                ->byStatus(CourseStatus::PUBLISHED->value)
+                ->whereHas('courseCategory', function ($categoryQuery) {
+                    $categoryQuery->where('is_active', true);
+                });
         }
 
         return $query;
